cdc073e8a4b35e3fa72f362bc60db31e8b928104
unknown <69911141maydu@gmail.com>
2024-10-28 18:41:24 +0530

    First commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,27 @@
 import "@/styles/globals.css";
+import Head from "next/head";
+import { ThemeProvider } from "@emotion/react";
+import { createTheme, CssBaseline } from "@mui/material";
+import Header from "@/Components/Header";
+import ThemeContext from "@/Providers/ThemeContext";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1976d2',
+    },
+    secondary: {
+      main: '#dc004e',
+
+    },
+  },
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return(
+    <ThemeContext>
+      <Header/>
+     <Component {...pageProps} />
+    </ThemeContext>
+    );
 }
