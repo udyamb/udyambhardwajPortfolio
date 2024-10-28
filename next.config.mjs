ea2033443d2f522aa954a8a0bcb101da498603e5
unknown <69911141maydu@gmail.com>
2024-10-28 18:46:00 +0530

    TOML file

--- next.config.mjs
+++ next.config.mjs
@@ -2,5 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
 };
-
+module.exports = {
+  target: 'serverless', // Ensures that the app is optimized for serverless deployment
+};
 export default nextConfig;
