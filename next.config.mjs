c68493b1362c70164a26567bd647154cd23b05b2
unknown <69911141maydu@gmail.com>
2024-10-28 19:19:01 +0530

    Resolved Module

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,5 @@
 const nextConfig = {
   reactStrictMode: true,
 };
-module.exports = {
-  target: 'serverless', // Ensures that the app is optimized for serverless deployment
-};
+
 export default nextConfig;
